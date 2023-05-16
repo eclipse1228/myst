ad89a0b90bbb430c6a16086f30fa6894f387a83e
ByeongSu Go <107296751+eclipse1228@users.noreply.github.com>
2023-05-16 13:03:27 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 import streamlit as st
+import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
-x = st.slider('select a value')
-st.write(x, 'squared is', x * x)
 
-rand= np.random.normal(1,2,size =20)
-fig , ax = plt.subplots()
-ax.hist(rand,bins =15)
-st.pyplot(fig)
+st.radio(’Pick’ [장애인복지관,시군구])
+
+st.selectbox(’장애인복지관’,[’male’,’시군구’])
+
+st.radio(’Year’,[2018,2019,2020,2021,2022])
+
+st.selectbox(’Year’,[2018,2019,2020,2021,2022])
+
+data = pd.read_csv(’C:\Users\고병수\Desktop\부산광역시 동구_장애인일자리 현황_20221024.csv’)
+
+plt.plot(data['월'], data['사람수'])
+plt.xlabel('월')
+plt.ylabel('')
+st.pyplot()
