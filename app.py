01590fbbbafdd14581e0787d8eef27162b6d18e7
ByeongSu Go <107296751+eclipse1228@users.noreply.github.com>
2023-05-16 13:20:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
+pip install streamlit pandas
+
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+# Streamlit app code
+st.title("Graph App")
 
-st.radio(’Pick’ [장애인복지관,시군구])
+# Sidebar input selection
+option = st.sidebar.selectbox("Select input", ['Welfare Center', 'Municipality'])
 
-st.selectbox(’장애인복지관’,[’male’,’시군구’])
+# Filter the data based on the selected option
+if option == 'Welfare Center':
+    filtered_data = data[data['Institution Type'] == 'Welfare Center for the Disabled']
+else:
+    filtered_data = data[data['Institution Type'] == 'Sigun-gu Busan Dong-gu Office']
 
-st.radio(’Year’,[2018,2019,2020,2021,2022])
-
-st.selectbox(’Year’,[2018,2019,2020,2021,2022])
-
-data = pd.read_csv(’C:\Users\고병수\Desktop\부산광역시 동구_장애인일자리 현황_20221024.csv’)
-
-plt.plot(data['월'], data['사람수'])
-plt.xlabel('월')
-plt.ylabel('')
+# Plotting the graph
+plt.plot(filtered_data['Year'], filtered_data['Assigned Personnel'])
+plt.xlabel('Year')
+plt.ylabel('Assigned Personnel')
 st.pyplot()
