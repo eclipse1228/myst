14ac29b130f4859be8ce85f83d5f5c98825a7dcf
ByeongSu Go <107296751+eclipse1228@users.noreply.github.com>
2023-05-18 17:41:26 +0900

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,42 @@ def main():
     st.sidebar.title("Chart Options")
 
     # Filter options
+    institution_types = df['Institutiimport pandas as pd
+import streamlit as st
+import matplotlib.pyplot as plt
+# Load the CSV file into a Pandas DataFrame
+
+
+
+
+df = pd.read_csv('job23.csv')
+
+def main():
+    st.title("Busan Jobs for Disabled People")
+    st.sidebar.title("Chart Options")
+
+    # Filter options
     institution_types = df['Institution Type'].unique()
+    selected_institution_type = st.sidebar.selectbox("Institution Type", institution_types)
+    
+
+    filtered_data = df[
+        (df['Institution Type'] == selected_institution_type) 
+    ]
+    
+    # Create chart
+    if not filtered_data.empty:
+        fig, ax = plt.subplots()
+        filtered_data.plot(x='year', y='Assigned Personnel', kind='bar', ax=ax)
+        plt.xlabel('year')
+        plt.ylabel('Assigned Personnel')
+        st.pyplot(fig)
+    else:
+        st.warning("No data available for the selected filters.")
+
+if __name__ == '__main__':
+    main()
+on Type'].unique()
     selected_institution_type = st.sidebar.selectbox("Institution Type", institution_types)
     
 
