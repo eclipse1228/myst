8e16343d87dd984ae0cbbcd5e1e00bde763e1bde
ByeongSu Go <107296751+eclipse1228@users.noreply.github.com>
2023-05-19 00:56:17 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,10 +10,9 @@ def main():
     st.sidebar.title("Chart Options")
 
     # Filter options
-    institution_types = ['시군구', '장애인복지관']
+    institution_types = df['Institution Type'].unique()
     selected_institution_type = st.sidebar.selectbox("Institution Type", institution_types)
-    show_average = st.sidebar.checkbox("Show Average")
-    show_ma = st.sidebar.checkbox("Show Moving Average")
+    compare_institutions = st.sidebar.checkbox("Compare Institutions")
 
     # Filter data for the selected institution type
     filtered_data = df[df['Institution Type'] == selected_institution_type]
@@ -21,32 +20,31 @@ def main():
     # Create chart
     if not filtered_data.empty:
         fig, ax = plt.subplots()
-
+        
         # Single institution graph
-        filtered_data.plot(x='year', y='Assigned Personnel', kind='bar', ax=ax)
-        plt.xlabel('Year')
-        plt.ylabel('Assigned Personnel')
-        plt.title(f"Personnel Distribution for {selected_institution_type}")
-
-        # Calculate moving average
-        if show_ma:
-            window = 2  # 이동 평균선의 기간 설정 (예시로 2년)
-            assigned_personnel = filtered_data['Assigned Personnel']
-
-            # 이동평균 계산
-            moving_average = assigned_personnel.rolling(window).mean()
-
-            # 이동평균선 그래프 그리기
-            ax.plot(filtered_data['year'], moving_average.shift(-window+1), color='green', linestyle='--', label=f'{window}-Year Moving Average')
-
-        # Calculate average and show average line
-        if show_average:
-            average_personnel = filtered_data.groupby('year')['Assigned Personnel'].mean()
-
-            # 평균선 그래프 그리기
-            ax.axhline(average_personnel.mean(), color='red', linestyle='---', label='Average')
-
-        plt.legend()
+        if not compare_institutions:
+            filtered_data.plot(x='year', y='Assigned Personnel', kind='bar', ax=ax)
+            plt.xlabel('Year')
+            plt.ylabel('Assigned Personnel')
+            plt.title(f"Personnel Distribution for {selected_institution_type}")
+        
+        # Comparison graph for two institutions
+        else:
+            institution1 = st.sidebar.selectbox("Select Institution 1", filtered_data['기관명'].unique())
+            institution2 = st.sidebar.selectbox("Select Institution 2", filtered_data['기관명'].unique())
+            
+            filtered_data1 = df[(df['Institution Type'] == selected_institution_type) & (df['기관명'] == institution1)]
+            filtered_data2 = df[(df['Institution Type'] == selected_institution_type) & (df['기관명'] == institution2)]
+            
+            if not filtered_data1.empty and not filtered_data2.empty:
+                fig, ax = plt.subplots()
+                ax.bar(filtered_data1['year'], filtered_data1['Assigned Personnel'], label=institution1)
+                ax.bar(filtered_data2['year'], filtered_data2['Assigned Personnel'], label=institution2)
+                plt.xlabel('Year')
+                plt.ylabel('Assigned Personnel')
+                plt.title(f"Personnel Comparison for {selected_institution_type}")
+                plt.legend()
+            
         st.pyplot(fig)
     else:
         st.warning("No data available for the selected filters.")
