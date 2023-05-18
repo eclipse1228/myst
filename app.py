a3d402f2ec3e4cef001b89dd59989d96848bfd89
ByeongSu Go <107296751+eclipse1228@users.noreply.github.com>
2023-05-18 16:35:59 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,32 +3,32 @@ import streamlit as st
 import matplotlib.pyplot as plt
 
 # Load the CSV file into a Pandas DataFrame
-df = pd.read_csv('job23.csv')
+df = pd.read_csv('job23.csv',encoding='utf-8')
 
 def main():
-    st.title("Institution Data Chart")
-    st.sidebar.title("Chart Options")
+    st.title("기관 데이터 차트")
+    st.sidebar.title("차트 옵션")
 
     # Filter options
     years = df['연도'].unique()
-    selected_year = st.sidebar.selectbox("Select Year", years)
+    selected_year = st.sidebar.selectbox("연도 선택", years)
 
     months = ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월']
-    selected_month = st.sidebar.selectbox("Select Month", months)
+    selected_month = st.sidebar.selectbox("월 선택", months)
 
     institution_types = df['기관유형'].unique()
-    selected_institution_type = st.sidebar.selectbox("Select Institution Type", institution_types)
+    selected_institution_type = st.sidebar.selectbox("기관유형 선", institution_types)
 
     filtered_data = df[
         (df['연도'] == selected_year) &
-        (df['1월'] == selected_month) &  # Replace 'Month' with 'Jan'
+        (df['선택된 월'] == selected_month) &  # Replace 'Month' with 'Jan'
         (df['기관유형'] == selected_institution_type)
     ]
 
     # Create chart
     if not filtered_data.empty:
         fig, ax = plt.subplots()
-        filtered_data.plot(x='Institution Name', y='Assigned Personnel', kind='bar', ax=ax)
+        filtered_data.plot(x='기관명', y='배정인원', kind='bar', ax=ax)
         plt.xlabel('기관명')
         plt.ylabel('배정인원')
         plt.title(f"Personnel Distribution of {selected_institution_type} in {selected_month} {selected_year}")
