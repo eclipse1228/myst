3ecf2726e917639556ef0a7491e3a5749a2d084b
ByeongSu Go <107296751+eclipse1228@users.noreply.github.com>
2023-05-18 23:17:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,8 @@ def main():
         # Show moving average line
         if show_ma:
             window = 2  # 이동 평균선의 기간 설정 (예시로 2년)
-            moving_average = filtered_data.groupby('year')['Assigned Personnel'].rolling(window).mean().reset_index(level=0, drop=True)
+            assigned_personnel = filtered_data['Assigned Personnel']
+            moving_average = assigned_personnel.rolling(window).mean()
             ax.plot(filtered_data['year'], moving_average, color='green', linestyle='--', label=f'{window}-Year Moving Average')
 
         plt.legend()
