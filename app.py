67f51b60b705be72ef4bd7cce9592d5c6afaf281
ByeongSu Go <107296751+eclipse1228@users.noreply.github.com>
2023-05-18 17:23:24 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
-import matplotlib as mpl
 # Load the CSV file into a Pandas DataFrame
 
 
@@ -16,9 +15,15 @@ def main():
     # Filter options
     institution_types = df['Institution Type'].unique()
     selected_institution_type = st.sidebar.selectbox("Institution Type", institution_types)
+    
+    months = ['1월', '2월', '3월', '4월', '5월']
+    selected_months = st.sidebar.multiselect("Select Months", months)
 
-    filtered_data = df[df['Institution Type'] == selected_institution_type]
-
+    filtered_data = df[
+        (df['Institution Type'] == selected_institution_type) &
+        (df['월'].isin(selected_months))
+    ]
+    
     # Create chart
     if not filtered_data.empty:
         fig, ax = plt.subplots()
